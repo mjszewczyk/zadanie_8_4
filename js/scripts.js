ebecb9439cce7f06ec1e4dbaf7a9c74bf90d3b22
Magdalena Szewczyk <mjszewczyk91@gmail.com>
2018-02-21 20:51:04 +0100

    Add scripts.js with comments

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+// deklaracja zmiennej name i wywołanie funkcji prompt
+var name = prompt('Enter your name');
+/*
+alert('Hello, ' + name); 
+*/
+// logowanie
+console.log('Hello, ' + name);
